ae8f7e95f6fff651b086901eb29f8efbbc2c9409
Luciano Silva <luciano2911@gmail.com>
2020-03-04 00:54:20 -0300

    jwt-decode

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { TokenService } from './../token/token.service';
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -11,7 +12,7 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService
+    private userService: UserService
   ) { }
 
   authenticate(userName: string, password: string) {
@@ -21,7 +22,7 @@ export class AuthService {
         { observe: 'response' })
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
         console.log(`User ${userName} authenticated with token: ${authToken}`);
       }));
   }
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { Subject } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
+
+import { User } from './user';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+    private userSubject = new Subject<User>();
+
+    constructor(private tokenService: TokenService) {
+
+        this.tokenService.hasToken() &&
+            this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() { }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+}
